5bb475a2dba9cfe7b536fc7c2b7e3bc43705adc5
Sebastian Olewczynski <olewczynski@gmail.com>
2018-07-19 22:51:35 +0200

    exercise done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
 
     public static void main(String args[]){
-        System.out.println("Hello, world!");
+        double poundsValue = 200d;
+        double conversionRate = 0.45359237;
+        double kilogramsValue = poundsValue * conversionRate;
+        System.out.println("converted from 1 pound to kg : " + kilogramsValue);
 
     }
 }
