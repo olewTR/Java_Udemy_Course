be280f2795915baa0f37515a99efa58df06e1924
Sebastian Olewczynski <olewczynski@gmail.com>
2018-07-25 22:02:42 +0200

    lecture 19

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,19 @@
 public class HelloWorld {
 
-    public static void main(String args[]){
-        double poundsValue = 200d;
-        double conversionRate = 0.45359237;
-        double kilogramsValue = poundsValue * conversionRate;
-        System.out.println("converted from 1 pound to kg : " + kilogramsValue);
+    public static void main(String args[]) {
+        char myChar = '&';  // w tej zmiennej można trzymać pojedynczy znak.
+        char myOtherChar = '\u00a9';  // w ten sposob przesylamy unicode char - ten to copyright
+
+        boolean myBoolean = true;
+        boolean myOtherBoolean = false; // tylko te dwie wartosci w tych zmiennych
+
+        // challenge for the video - find the code for "registered" unicode character
+        // and print it:
+
+        char unicodeCharForRegisteredChar = '\u00AE';
+
+        System.out.println(unicodeCharForRegisteredChar);
+
 
     }
 }
