1dee9830db9869e00505b151a68224ecf2374a40
Sebastian Olewczynski <olewczynski@gmail.com>
2018-10-23 22:08:10 +0200

    lekcja z operatorami

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -26,5 +26,33 @@ public class HelloWorld {
 
         lastString = lastString + myInt; // int przeksztalcony w stringa
         System.out.println(lastString);
+
+        // tutaj powtorka z ternary operatora;
+        boolean isCar = true;
+
+        boolean wasCar = isCar ? true : false; // jezeli warunek true, to pierwszy, jezeli nie, to drugi
+
+        if(wasCar)
+            System.out.println("yea, car was");
+
+
+        // challenge here ! :
+        // 1. create a double variable with the value 20
+        // 2. Create a second variable of type double with the value 80
+        // 3. Add both numbers up and multiply by 25
+        // 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        // 5. Write an "if" statement that displays a message "Total was over the limit"
+        // if the remaining total (#4) is equal to 20 or less
+
+        double challengeVarOne = 20;
+        double challengeVarTwo = 80;
+        double result = (challengeVarOne + challengeVarTwo) * 25;
+        System.out.println(result + " this is the result");
+        double reminderResult = result % 40;
+        System.out.println(reminderResult + " this is the reminder result");
+        if(reminderResult <= 20)
+        {
+            System.out.println("Total was over the limit");
+        }
     }
 }
